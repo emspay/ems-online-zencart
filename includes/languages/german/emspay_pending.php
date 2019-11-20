9aef636fe9a50051a61a0780745f70692062450e
sorshireality <tutunikssa@gmail.com>
2019-11-20 15:03:04 +0200

    french,german languages

--- includes/languages/german/emspay_pending.php
+++ includes/languages/german/emspay_pending.php
@@ -1,7 +1,7 @@
 <?php
 
 define('NAVBAR_TITLE', 'Ihre Bestellung wird bearbeitet');
-define('INGPSP_PLEASE_WAIT', 'Bitte warten Sie, während Ihr Bestellstatus überprüft wird.');
-define('INGPSP_ORDER_PROCESSING', 'Ihre Bestellung wird gerade bearbeitet.');
-define('INGPSP_ORDER_PENDING', 'Wir haben noch keine Bestätigung von Ihrer Bank oder Ihrem Kartenaussteller erhalten.');
-define('INGPSP_ORDER_PENDING_MESSAGE', 'Sie erhalten eine Nachricht, sobald wir diese erhalten haben.');
+define('EMSPAY_PLEASE_WAIT', 'Bitte warten Sie, während Ihr Bestellstatus überprüft wird.');
+define('EMSPAY_ORDER_PROCESSING', 'Ihre Bestellung wird gerade bearbeitet.');
+define('EMSPAY_ORDER_PENDING', 'Wir haben noch keine Bestätigung von Ihrer Bank oder Ihrem Kartenaussteller erhalten.');
+define('EMSPAY_ORDER_PENDING_MESSAGE', 'Sie erhalten eine Nachricht, sobald wir diese erhalten haben.');
--- includes/languages/german/modules/payment/emspay.php
+++ includes/languages/german/modules/payment/emspay.php
@@ -1,40 +1,40 @@
 <?php
 
-define('MODULE_PAYMENT_INGPSP_TEXT_TITLE', "ING PSP");
-define('MODULE_PAYMENT_INGPSP_TEXT_DESCRIPTION', "Offizielle ING PSP ZenCart-Plugin");
+define('MODULE_PAYMENT_EMSPAY_TEXT_TITLE', "EMS PAY");
+define('MODULE_PAYMENT_EMSPAY_TEXT_DESCRIPTION', "Offizielle EMS PAY ZenCart-Plugin");
 
-define('MODULE_PAYMENT_INGPSP_STATUS_TEXT', "Aktivieren ING PSP-Modul");
-define('MODULE_PAYMENT_INGPSP_STATUS_DESCRIPTION', "Wollen Sie ING PSP ZenCart Modul aktivieren?");
+define('MODULE_PAYMENT_EMSPAY_STATUS_TEXT', "Aktivieren EMS PAY-Modul");
+define('MODULE_PAYMENT_EMSPAY_STATUS_DESCRIPTION', "Wollen Sie EMS PAY ZenCart Modul aktivieren?");
 
-define('MODULE_PAYMENT_INGPSP_API_KEY_TEXT', "API Schlüssel");
-define('MODULE_PAYMENT_INGPSP_API_KEY_DESCRIPTION', "Duplizieren Sie Ihre ING PSP API Schlüssel von Merchant Portal.");
+define('MODULE_PAYMENT_EMSPAY_API_KEY_TEXT', "API Schlüssel");
+define('MODULE_PAYMENT_EMSPAY_API_KEY_DESCRIPTION', "Duplizieren Sie Ihre EMS PAY API Schlüssel von Merchant Portal.");
 
-define('MODULE_PAYMENT_INGPSP_PRODUCT_TEXT', "ING PSP Produkt");
-define('MODULE_PAYMENT_INGPSP_PRODUCT_DESCRIPTION', "Wählen Sie Ihres ING PSP Produkt.");
+define('MODULE_PAYMENT_EMSPAY_PRODUCT_TEXT', "EMS PAY Produkt");
+define('MODULE_PAYMENT_EMSPAY_PRODUCT_DESCRIPTION', "Wählen Sie Ihres EMS PAY Produkt.");
 
-define('MODULE_PAYMENT_INGPSP_BUNDLE_CA_TEXT', "cURL CA bündel benutzen");
-define('MODULE_PAYMENT_INGPSP_BUNDLE_CA_DESCRIPTION', "Löst Probleme, wenn curl.cacert Pfad nicht in PHP.ini eingestellt wird.");
+define('MODULE_PAYMENT_EMSPAY_BUNDLE_CA_TEXT', "cURL CA bündel benutzen");
+define('MODULE_PAYMENT_EMSPAY_BUNDLE_CA_DESCRIPTION', "Löst Probleme, wenn curl.cacert Pfad nicht in PHP.ini eingestellt wird.");
 
-define('MODULE_PAYMENT_INGPSP_WEBHOOK_TEXT', "Webhook URL generieren");
-define('MODULE_PAYMENT_INGPSP_WEBHOOK_DESCRIPTION', "Verwende automatisch der Webhook URL mit jeder Bestellung zum ING PSP API.");
+define('MODULE_PAYMENT_EMSPAY_WEBHOOK_TEXT', "Webhook URL generieren");
+define('MODULE_PAYMENT_EMSPAY_WEBHOOK_DESCRIPTION', "Verwende automatisch der Webhook URL mit jeder Bestellung zum EMS PAY API.");
 
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_COMPLETED_TEXT', "Status für eine abgeschlossene Bestellung");
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_COMPLETED_DESCRIPTION', "Wählen Sie den Status aus der Liste der Sie anwenden möchten für abgeschlossene Bestellungen.");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_COMPLETED_TEXT', "Status für eine abgeschlossene Bestellung");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_COMPLETED_DESCRIPTION', "Wählen Sie den Status aus der Liste der Sie anwenden möchten für abgeschlossene Bestellungen.");
 
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_PENDING_TEXT', "Status für eine ausstehende Bestellung");
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_PENDING_DESCRIPTION', "Wählen Sie den Status aus der Liste der Sie anwenden möchten für ausstehende Bestellungen.");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_PENDING_TEXT', "Status für eine ausstehende Bestellung");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_PENDING_DESCRIPTION', "Wählen Sie den Status aus der Liste der Sie anwenden möchten für ausstehende Bestellungen.");
 
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_ERROR_TEXT', "Status für eine fehlgeschlagen Bestellung");
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_ERROR_DESCRIPTION', "Wählen Sie den Status der Sie anwenden möchten für fehlgeschlagen Bestellungen.");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_ERROR_TEXT', "Status für eine fehlgeschlagen Bestellung");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_ERROR_DESCRIPTION', "Wählen Sie den Status der Sie anwenden möchten für fehlgeschlagen Bestellungen.");
 
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_PROCESSING_TEXT', "Status für eine Bestellung in Verarbeitung");
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_PROCESSING_DESCRIPTION', "Wählen Sie den Status der Sie anwenden möchten für Bestellungen die verarbeitet werden.");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_PROCESSING_TEXT', "Status für eine Bestellung in Verarbeitung");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_PROCESSING_DESCRIPTION', "Wählen Sie den Status der Sie anwenden möchten für Bestellungen die verarbeitet werden.");
 
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_CANCELLED_TEXT', "Status für eine annullierte Bestellung");
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_CANCELLED_DESCRIPTION', "Wählen Sie den Status aus der Liste der Sie anwenden möchten für annulierte Bestellungen.");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_CANCELLED_TEXT', "Status für eine annullierte Bestellung");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_CANCELLED_DESCRIPTION', "Wählen Sie den Status aus der Liste der Sie anwenden möchten für annulierte Bestellungen.");
 
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_SHIPPED_TEXT', "Status für eine erfasste/ausgelieferte Bestellung");
-define('MODULE_PAYMENT_INGPSP_ORDER_STATUS_SHIPPED_DESCRIPTION', "Wählen Sie den Status der Sie anwenden möchten für erfasste/ausgelieferte Bestellungen.");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_SHIPPED_TEXT', "Status für eine erfasste/ausgelieferte Bestellung");
+define('MODULE_PAYMENT_EMSPAY_ORDER_STATUS_SHIPPED_DESCRIPTION', "Wählen Sie den Status der Sie anwenden möchten für erfasste/ausgelieferte Bestellungen.");
 
-define('MODULE_PAYMENT_INGPSP_ERROR_API_KEY', "Der API-Schlüssel wurde nicht festgelegt. Bitte geben Sie einen gültigen API-Schlüssel ein.");
-define('MODULE_PAYMENT_INGPSP_ERROR_TRANSACTION', "Leider ist ein Fehler bei der Verarbeitung Ihrer Bezahlung aufgetreten. Bitte versuchen Sie nochmals.");
+define('MODULE_PAYMENT_EMSPAY_ERROR_API_KEY', "Der API-Schlüssel wurde nicht festgelegt. Bitte geben Sie einen gültigen API-Schlüssel ein.");
+define('MODULE_PAYMENT_EMSPAY_ERROR_TRANSACTION', "Leider ist ein Fehler bei der Verarbeitung Ihrer Bezahlung aufgetreten. Bitte versuchen Sie nochmals.");
